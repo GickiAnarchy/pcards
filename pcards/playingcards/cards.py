aa0e29725490c72e2d3b90123c65784c5c7a71d7
GickiAnarchy <mindofmagick85@gmail.com>
2022-03-05 01:30:06 -0600

    1b

--- pcards/playingcards/cards.py
+++ pcards/playingcards/cards.py
@@ -4,9 +4,43 @@ import random
 
 class Card:
   """Single playing card class"""
-  
-  
-  
-  
+
+  suits = ["spades", "hearts", "diamonds", "clubs"]
+
+  values = [None, None,"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
   def __init__(self, s, v):
-    pass
+    """suit + value are ints"""
+    self.value = v
+    self.suit = s
+
+
+  def __lt__(self, c2):
+    """Handles the 'less than' operator (<)"""
+    if self.value < c2.value:
+      return True
+    if self.value == c2.value:
+      if self.suit < c2.suit:
+        return True
+      else:
+        return False
+    return False
+
+  def __gt__(self, c2):
+    """Handles the 'greater than' operator (>)"""
+    if self.value > c2.value:
+      return True
+    if self.value == c2.value:
+      if self.suit > c2.suit:
+        return True
+      else:
+        return False
+    return False
+
+  def __repr__(self):
+    """Returns a string format of the object"""
+    v = self.values[self.value] +\
+      " of " + \
+      self.suits[self.suit]
+    return v

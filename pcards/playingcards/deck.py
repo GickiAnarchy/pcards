928fae5fa3953e131b0277f6c0f21b78f32a330a
GickiAnarchy <mindofmagick85@gmail.com>
2022-03-07 09:14:29 -0600

    2b

--- pcards/playingcards/deck.py
+++ pcards/playingcards/deck.py
@@ -1,7 +1,34 @@
 #deck.py
 class Deck:
-    """Deck of 52 Playing Cards"""
-    
+    """ Class of 52 Card() to create a full deck."""
     def __init__(self):
         self.cards = []
-        
+    
+    def createDeck(self):
+        for i in range(2, 15):
+            for j in range(4):
+                crd = Card(i, j)
+                self.cards.append(crd)
+        self.shuffle()
+
+    def shuffle(self):
+        if len(self.cards) == 0:
+            return
+        shuffle(self.cards)
+        print(f"Shuffling {str(len(self.cards))} cards..")
+
+    def drawCard(self):
+        if len(self.cards) == 0:
+            return
+        pulled = self.cards.pop()
+        print(f"Pulled card:\n{pulled}")
+        return
+    
+    def drawHand(self, amount):
+        self.hand = []
+        while amount > 0:
+            self.hand.append(self.drawCard())
+        print(f"Dealt a hand of {str(amount)} cards")
+        return self.hand
+    
+    
